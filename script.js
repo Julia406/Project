b82e024bd958892ad1ae93c1eca1cac462f257ad
julia406 <julia40688@gmail.com>
2018-08-16 21:35:18 +0300

    instal gulp eslin

---
+++ script.js
@@ -0,0 +1,13 @@
+(function () {
+	var group = document.getElementsByClassName('group')[0];
+
+	window.addCard = function () {
+
+		var defaultCard = document.createElement('div');
+		defaultCard.className = 'card';
+		var lastChild = group.children[group.children.length - 1];
+		group.insertBefore(defaultCard, lastChild);
+	};
+}());
+
+
